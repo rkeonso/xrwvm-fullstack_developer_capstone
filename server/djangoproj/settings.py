039b58bfa56c3825cb6eb7ae331a6a1e2681d505
rkeonso <93288761+rkeonso@users.noreply.github.com>
2025-12-04 22:43:30 -0600

    Fix CSRF_TRUSTED_ORIGINS and add newline at EOF

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -17,7 +17,7 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://rkeonso-8000.theiadockernext-0-labs-prod-"
-    "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    "theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 
 REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": []}
