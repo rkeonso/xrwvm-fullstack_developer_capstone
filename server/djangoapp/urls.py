dce3d4bd013e2219a64eaf8d653122b4e0560dd4
rkeonso <93288761+rkeonso@users.noreply.github.com>
2026-06-08 00:16:19 -0500

    Fix indentation for dealer reviews URL path

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('dealers/', views.get_dealerships, name='get_dealerships'),
     path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
-    path('dealers/<int:dealer_id>/reviews', 
+    path('dealers/<int:dealer_id>/reviews',
          views.get_dealer_reviews, name='get_dealer_reviews'),
     path('add_review', views.add_review, name='add_review'),
     path('register', views.registration, name='register'),
